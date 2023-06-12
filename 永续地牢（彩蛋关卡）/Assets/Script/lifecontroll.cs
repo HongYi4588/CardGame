49653f8db94a92af44bafb67f666b5fd92d4efdf
HongYi4588 <128110789+HongYi4588@users.noreply.github.com>
2023-06-13 01:46:08 +0800

    永续地牢彩蛋关卡_显示生命脚本
    收到攻击减一

--- 永续地牢（彩蛋关卡）/Assets/Script/lifecontroll.cs
+++ 永续地牢（彩蛋关卡）/Assets/Script/lifecontroll.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 public class lifecontroll : MonoBehaviour
 {
-    public Sprite[] lifeimage;
-    public GameObject player;
-    private int currentlife;
-    public Image lifeage;
+
+public GameObject player;
+public Sprite[] lifeimage;
+private int currentlife;
+public Image lifeage;
     void Update()
     {
-       
         currentlife = player.GetComponent<PlayerControll>().life;
         if (currentlife > -1)
         {
-            lifeage.sprite = lifeimage[currentlife];
-        }
+            lifeage.sprite = lifeimage[currentlife];}
     }
 
 }
