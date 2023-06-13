fdead99d0be7e803b44466352abe167c5b5dd99b
HongYi4588 <128110789+HongYi4588@users.noreply.github.com>
2023-06-13 10:02:49 +0800

    永续地牢_彩蛋关卡 UI交互脚本
    点击开始游戏，推出游戏，返回主界面等操作

--- 永续地牢（彩蛋关卡）/Assets/Script/UI/SceneControll.cs
+++ 永续地牢（彩蛋关卡）/Assets/Script/UI/SceneControll.cs
@@ -7,30 +7,30 @@ public class SceneControll : MonoBehaviour
 {
     public void Begin()
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene(1);
+     Time.timeScale = 1;
+     SceneManager.LoadScene(1);
     }
     public void Quit()
     {
-        Application.Quit();
+     Application.Quit();
     }
     public void Home()
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene(0);
+     Time.timeScale = 1;
+     SceneManager.LoadScene(0);
     }
     public void StopGame()
     {
-        Debug.Log("stopgame");
-        Time.timeScale = 0;
+    Debug.Log("stopgame");
+     Time.timeScale = 0;
     }
     public void ContinGame()
     {
-        Time.timeScale = 1;
+     Time.timeScale = 1;
     }
     public void Level2()
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene(2);
+     Time.timeScale = 1;
+     SceneManager.LoadScene(2);
     }
 }
